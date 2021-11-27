29484d5b2080d8c112752928546ea888bcf6d91f
Cristian Florea <79513347+FloreaCT@users.noreply.github.com>
2021-11-27 16:28:27 +0000

    Update magic8.py

--- 2-control-flow/magic-8-ball/magic8.py
+++ 2-control-flow/magic-8-ball/magic8.py
@@ -1,32 +1,42 @@
 import random
 
-name = "Joe"
-question = "Will I win the lottery?"
+name = "Cristi"
+question = "Will earth be alright in 20 years?"
 answer = ""
 
-random_number = random.randint(1, 9)
-# print(random_number)
+random_number = random.randint(1,12)
+
+if name == "":
+  print("Question:", question)
+else:
+  print(name, "asks:", question)
 
 if random_number == 1:
-  answer = "Yes - definitely"
+  answer = "Yes - definitely."
 elif random_number == 2:
-  answer = "It is decidedly so"
+  answer = "It is decidedly so."
 elif random_number == 3:
   answer = "Without a doubt"
 elif random_number == 4:
-  answer = "Reply hazy, try again"
+  answer = "Reply hazy, try again."
 elif random_number == 5:
-  answer = "Ask again later"
+  answer = "Ask again later."
 elif random_number == 6:
-  answer = "Better not tell you now"
+  answer = "Better not tell you now."
 elif random_number == 7:
-  answer = "My sources say no"
+  answer = "My sources say no."
 elif random_number == 8:
-  answer = "Outlook not so good"
+  answer = "Outlook not so good."
 elif random_number == 9:
-  answer = "Very doubtful"
+  answer = "Very doubtful."
+elif random_number == 10:
+  answer = "The answer is inside you"
+elif random_number == 11:
+  answer = "If it's ment to be, it will be."
 else:
   answer = "Error"
-  
-print(name + " asks: " + question)
-print("Magic 8 Ball's answer: " + answer)
+
+if question == "":
+  print("The fabric of reality is at risk! Please provide a question!")
+else:
+  print("Magic 8-Ball's answer:", answer)
